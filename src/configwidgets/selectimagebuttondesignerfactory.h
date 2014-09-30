9ebf702cfb21acbb3f798480f831918bc9497f75
David Edmundson <kde@davidedmundson.co.uk>
2014-09-30 14:36:51 +0200

    Relicense one of my files to be GPL2+ formerly GPLv3

--- src/configwidgets/selectimagebuttondesignerfactory.h
+++ src/configwidgets/selectimagebuttondesignerfactory.h
@@ -1,21 +1,19 @@
 /*
-This file is part of LightDM-KDE.
+    Copyright 2011,2012 by David Edmundson <davidedmundson@kde.org>
 
-Copyright 2011, 2012 David Edmundson <kde@davidedmundson.co.uk>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
 
-LightDM-KDE is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-LightDM-KDE is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with LightDM-KDE.  If not, see <http://www.gnu.org/licenses/>.
-*/
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef SELECTIMAGEBUTTONDESIGNERFACTORY_H
 #define SELECTIMAGEBUTTONDESIGNERFACTORY_H
