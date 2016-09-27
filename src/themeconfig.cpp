645ba842a18b51f403e5bb11851dbbecc3e951ff
David Edmundson <kde@davidedmundson.co.uk>
2016-09-27 17:15:38 +0100

    If the theme set in the SDDM config is missing, select first possible entry
    Summary:
    It's possible for the SDDM config to not list a theme, particularly if you're using
    the default theme.
    
    Test Plan: Set a blank SDDM config. Opened the KCM, it looked sane.
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D2876

--- src/themeconfig.cpp
+++ src/themeconfig.cpp
@@ -86,11 +86,11 @@ QString ThemeConfig::themeConfigPath() const
 void ThemeConfig::prepareInitialTheme()
 {
     const QString initialTheme = mConfig->group("Theme").readEntry("Current");
-    
-    const QModelIndex index = findThemeIndex(initialTheme);
-    if (!index.isValid()) {
-        //KMessageBox::error(this, i18n("Could not find any themes. \nPlease install SDDM themes."), i18n("No SDDM themes"));
-        return;
+
+    QModelIndex index = findThemeIndex(initialTheme);
+    if (!index.isValid() && configUi->themesListView->model()->rowCount() > 0) {
+        //if we can't find the currently configured theme from the config, just select the first theme in the UI
+        index = configUi->themesListView->model()->index(0,0);
     }
     configUi->themesListView->setCurrentIndex(index);
     themeSelected(index);
