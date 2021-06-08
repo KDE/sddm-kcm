0329e1f1aab75999f2cfe966a10f115a97280e30
David Redondo <kde@david-redondo.de>
2021-06-08 08:52:07 +0200

    Read correct defaults
    KConfig considers additional config files to provide defaults. This
    is not what we want in our case. As a workaround only add them after
    the skeleton constructor that adds the items to itself which reads
    the default.

--- src/sddmdata.cpp
+++ src/sddmdata.cpp
@@ -10,17 +10,24 @@
 #include <KSharedConfig>
 
 #include <QDir>
+#include <QTemporaryFile>
 
 SddmData::SddmData(QObject *parent, const QVariantList &args)
     : KCModuleData(parent, args)
 {
+    // The defaults should be read from the defaults config (see sddmsettingsbase.h) but KConfig
+    // considers everything but the main file to provide defaults. To work around reading of wrong
+    // defaults we provide an empty dummy config so we read the correct defaults. Afterwards the
+    // current values are reread from the actual config files.
+    m_settings = new SddmSettings(KSharedConfig::openConfig(QTemporaryFile().fileName()), this);
     auto config = KSharedConfig::openConfig(QStringLiteral(SDDM_CONFIG_FILE), KConfig::CascadeConfig);
     QStringList configFiles = QDir(QStringLiteral(SDDM_CONFIG_DIR)).entryList(QDir::Files | QDir::NoDotAndDotDot, QDir::LocaleAware);
     std::transform(configFiles.begin(), configFiles.end(), configFiles.begin(), [](const QString &filename) -> QString {
         return QStringLiteral(SDDM_CONFIG_DIR "/") + filename;
     });
     config->addConfigSources(configFiles);
-    m_settings = new SddmSettings(config, this);
+    m_settings->setSharedConfig(config);
+    m_settings->read();
     autoRegisterSkeletons();
 }
 
