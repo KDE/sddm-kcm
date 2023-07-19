KDE/sddm-kcm

commit 586ee8078bd06c74e61da2f09cd5ea4a62346ce4

Add spacing values to all Layout objects
We don't want to use the default value of 5px, but rather a standard
unit we control.

src/ui/Advanced.qml
@@ -18,8 +18,12 @@ Kirigami.Page {
 
     Kirigami.FormLayout {
         width: parent.width
+        spacing: Kirigami.Units.smallSpacing
+
         RowLayout {
             Kirigami.FormData.label: i18nc("option:check", "Automatically log in:")
+            spacing: Kirigami.Units.smallSpacing
+
             QQC2.CheckBox {
                 id: autologinBox
                 text: i18nc("@label:listbox, the following combobox selects the user to log in automatically", "as user:")
@@ -153,6 +157,8 @@ To avoid this, you can change the wallet to have a blank password. Note that thi
         RowLayout {
             Kirigami.FormData.label: i18nc("@label:textbox", "Halt Command:")
             Layout.fillWidth: true
+            spacing: Kirigami.Units.smallSpacing
+
             Kirigami.ActionTextField {
                 id: haltField
                 Layout.fillWidth: true
@@ -183,6 +189,8 @@ To avoid this, you can change the wallet to have a blank password. Note that thi
         RowLayout {
             Layout.fillWidth: true
             Kirigami.FormData.label: i18nc("@label:textbox", "Reboot Command:")
+            spacing: Kirigami.Units.smallSpacing
+
             Kirigami.ActionTextField {
                 id: rebootField
                 Layout.fillWidth: true

src/ui/DetailsDialog.qml
@@ -27,6 +27,8 @@ Kirigami.OverlaySheet {
     ColumnLayout {
         id: layout
         Layout.preferredWidth: 0.75 * root.width
+        spacing: Kirigami.Units.smallSpacing
+
         Item {
             // we need this wrapper because of a limitation of Image where the implicitHeight is not adjusted when
             // the height has been set as the layout does

src/ui/main.qml
@@ -141,6 +141,8 @@ KCM.GridViewKCM {
                 i18n("Please note that theme files must be installed globally to be reflected on the SDDM login screen.")
         }
         footer: RowLayout {
+            spacing: Kirigami.Units.smallSpacing
+
             Item {
                 Layout.fillWidth: true
             }
@@ -190,6 +192,8 @@ KCM.GridViewKCM {
             }
         }
         footer: RowLayout {
+            spacing: Kirigami.Units.smallSpacing
+
             Item {
                 Layout.fillWidth: true
             }

commit c570b153d5885fb91cafff0f349cc81bccc88be4

Remove inappropriate spacing property on FormLayout
Fixup for 586ee8078bd06c74e61da2f09cd5ea4a62346ce4, which introduced
this error.

src/ui/Advanced.qml
@@ -18,7 +18,6 @@ Kirigami.Page {
 
     Kirigami.FormLayout {
         width: parent.width
-        spacing: Kirigami.Units.smallSpacing
 
         RowLayout {
             Kirigami.FormData.label: i18nc("option:check", "Automatically log in:")