c0100486ea0e8251a5e9d0a80d7f696920f4965c
David Edmundson <kde@davidedmundson.co.uk>
2026-01-07 10:38:12 +0000

    kcm: Hide when SDDM is not enabled

--- src/sddmdata.cpp
+++ src/sddmdata.cpp
@@ -9,11 +9,32 @@
 
 #include <KSharedConfig>
 
+#include <QDBusConnection>
+#include <QDBusMessage>
+#include <QDBusReply>
 #include <QDir>
 
 SddmData::SddmData(QObject *parent)
     : KCModuleData(parent)
 {
+    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.systemd1"),
+                                                      QStringLiteral("/org/freedesktop/systemd1"),
+                                                      QStringLiteral("org.freedesktop.systemd1.Manager"),
+                                                      QStringLiteral("GetUnitFileState"));
+    msg << QStringLiteral("sddm.service");
+
+    // it seems system settings wants things to be sync
+    // It's not like systemd will ever be down
+    QDBusReply<QString> hasSddmReply = QDBusConnection::systemBus().call(msg);
+    // a quirk is that if sddm is uninstalled systemd replies with an error of invalid args
+    if (hasSddmReply.value() == QLatin1String("enabled")) {
+        setRelevant(true);
+    } else if (hasSddmReply.error().type() == QDBusError::ServiceUnknown) { // user isn't installing systemd, we can't query it
+        setRelevant(true);
+    } else {
+        setRelevant(false);
+    }
+
     auto config = KSharedConfig::openConfig(QStringLiteral(SDDM_CONFIG_FILE), KConfig::CascadeConfig);
     QStringList configFiles = QDir(QStringLiteral(SDDM_CONFIG_DIR)).entryList(QDir::Files | QDir::NoDotAndDotDot, QDir::LocaleAware);
     std::transform(configFiles.begin(), configFiles.end(), configFiles.begin(), [](const QString &filename) -> QString {
